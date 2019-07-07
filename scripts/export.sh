a709d22b500986068a2f3761a9b3df7503ea178f
Ceri Storey <cez@necrofish.org.uk>
2019-07-07 11:41:38 +0100

    Compress export using zstd instead.

--- scripts/export.sh
+++ scripts/export.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env nix-shell
-#! nix-shell -p xz -p nix -p findutils -i bash
+#! nix-shell -p zstd -p nix -p coreutils -p findutils -i bash
 
 set -xeuo pipefail
 
 input=$1
 output=$2
 
+tmpf=$(mktemp /tmp/out-XXXXXXX)
+
 nix-store -qR "$input" |  \
     tee /dev/stderr | \
     xargs nix-store --export | \
-    xz -v > "$output"
+    zstd -9v > "$tmpf"
+mv -v "$tmpf" "$output".zst
